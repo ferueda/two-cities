de94b7f0eb4632a4a5cb08ef571945cce42e9ec8
ferueda <fe.rueda@outlook.com>
2020-03-07 18:49:03 -0300

    added dropdown and search functionality

--- script.js
+++ script.js
@@ -1,7 +1,86 @@
+const cityOneInput = document.getElementById('city-one');
+const cityTwoInput = document.getElementById('city-two');
+const cityOneContainer = document.getElementById('city-one-container');
+const cityTwoContainer = document.getElementById('city-Two-container');
+const cityOneSuggestions = document.getElementById('city-one-suggestions');
+const cityTwoSuggestions = document.getElementById('city-two-suggestions');
 const arrowBtn = document.getElementById('arrow');
+
+let cities = [];
+
+async function getData() {
+  const res = await fetch('world-cities_json.json');
+  const data = await res.json();
+  cities = data;
+}
+
+// city one listeners
+
+cityOneInput.addEventListener('input', () => {
+  const input = cityOneInput.value.toLowerCase();
+  let filteredCities = cities.filter(
+    city =>
+      city['name'].toLowerCase().startsWith(input) ||
+      city['country'].toLowerCase().includes(input)
+  );
+
+  cityOneSuggestions.innerHTML = '';
+
+  filteredCities.slice(0, 10).forEach(city => {
+    const cityEl = document.createElement('div');
+    cityEl.innerText = `${city['name']}, ${city.country}`;
+    cityOneSuggestions.appendChild(cityEl);
+  });
+
+  if (input === '') {
+    cityOneSuggestions.innerHTML = '';
+  }
+});
+
+cityOneSuggestions.addEventListener('click', e => {
+  cityOneInput.value = e.target.innerText;
+  cityOneSuggestions.innerHTML = '';
+});
+
+// city two listeners
+
+cityTwoInput.addEventListener('input', () => {
+  const input = cityTwoInput.value.toLowerCase();
+  let filteredCities = cities.filter(
+    city =>
+      city['name'].toLowerCase().startsWith(input) ||
+      city['country'].toLowerCase().includes(input)
+  );
+
+  cityTwoSuggestions.innerHTML = '';
+
+  filteredCities.slice(0, 10).forEach(city => {
+    const cityEl = document.createElement('div');
+    cityEl.innerText = `${city['name']}, ${city.country}`;
+    cityTwoSuggestions.appendChild(cityEl);
+  });
+
+  if (input === '') {
+    cityTwoSuggestions.innerHTML = '';
+  }
+});
+
+cityTwoSuggestions.addEventListener('click', e => {
+  cityTwoInput.value = e.target.innerText;
+  cityTwoSuggestions.innerHTML = '';
+});
+
+// swap button functions
+
 let rotated = false;
 
 arrowBtn.addEventListener('click', () => {
   arrowBtn.style.transform = rotated ? 'rotate(0deg)' : 'rotate(180deg)';
   rotated = !rotated;
+  [cityOneInput.value, cityTwoInput.value] = [
+    cityTwoInput.value,
+    cityOneInput.value
+  ];
 });
+
+getData();
