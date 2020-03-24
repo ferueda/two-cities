e756e99c34627b8b38d2c5e09b0ae5a4d73d3a89
ferueda <fe.rueda@outlook.com>
2020-03-24 13:36:47 -0300

    refactored fetch requests into async await funcs

--- requests.js
+++ requests.js
@@ -1,141 +1,165 @@
-// const getCitiesData = async () => {
-//   const res = await fetch('world-cities_json.json');
-//   return await res.json();
+// const getCitiesData = () => {
+//   return fetch('world-cities_json.json').then(res => {
+//     if (res.status === 200) {
+//       return res.json();
+//     } else {
+//       throw new Error('Unable to fetch cities data');
+//     }
+//   });
 // };
 
-const getCitiesData = () => {
-  return fetch('world-cities_json.json').then(res => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      throw new Error('Unable to fetch cities data');
-    }
-  });
+const getCitiesData = async () => {
+  const response = await fetch('world-cities_json.json');
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    throw new Error('Unable to fetch cities data');
+  }
 };
 
-// const getCountryData = async country => {
-//   const res = await fetch('https://restcountries.eu/rest/v2/all');
-
-//   const data = await res.json();
-//   return data.find(obj => obj.name === country || obj.nativeName === country);
+// const getCountryData = country => {
+//   return fetch('https://restcountries.eu/rest/v2/all')
+//     .then(res => {
+//       if (res.status === 200) {
+//         return res.json();
+//       } else {
+//         throw new Error('Unable to fetch countries data');
+//       }
+//     })
+//     .then(data => {
+//       return data.find(
+//         obj => obj.name === country || obj.nativeName === country
+//       );
+//     });
 // };
 
-const getCountryData = country => {
-  return fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error('Unable to fetch countries data');
-      }
-    })
-    .then(data => {
-      return data.find(
-        obj => obj.name === country || obj.nativeName === country
-      );
-    });
+const getCountryData = async country => {
+  const response = await fetch('https://restcountries.eu/rest/v2/all');
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.find(obj => obj.name === country || obj.nativeName === country);
+  } else {
+    throw new Error('Unable to fetch countries data');
+  }
 };
 
-// async function getExchangeRate(base, target) {
-//   const res = await fetch(
+// const getExchangeRate = (base, target) => {
+//   return fetch(
 //     `https://openexchangerates.org/api/latest.json?app_id=50b9e063716540c3b23c99c33e83bbb2&base=USD`
-//     );
-//   const data = await res.json();
-//   return data.rates[target] / data.rates[base];
-// }
+//   )
+//     .then(res => {
+//       if (res.status === 200) {
+//         return res.json();
+//       } else {
+//         throw new Error('Unable to fetch exchange rate data');
+//       }
+//     })
+//     .then(data => {
+//       return data.rates[target] / data.rates[base];
+//     });
+// };
 
-const getExchangeRate = (base, target) => {
-  return fetch(
+const getExchangeRate = async (base, target) => {
+  const response = await fetch(
     `https://openexchangerates.org/api/latest.json?app_id=50b9e063716540c3b23c99c33e83bbb2&base=USD`
-  )
-    .then(res => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error('Unable to fetch exchange rate data');
-      }
-    })
-    .then(data => {
-      return data.rates[target] / data.rates[base];
-    });
+  );
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.rates[target] / data.rates[base];
+  } else {
+    throw new Error('Unable to fetch exchange rates data');
+  }
 };
 
-// async function getLatAndLong(city, country) {
-//   const res = await fetch(
+// const getLatAndLon = (city, country) => {
+//   return fetch(
 //     `https://api.opencagedata.com/geocode/v1/json?q=${city}+${country}&key=1329eac7d692480f865fcecd32eee0ae&pretty=1`
-//   );
-//   const data = await res.json();
-//   return data['results'][0]['geometry'];
-// }
+//   )
+//     .then(res => {
+//       if (res.status === 200) {
+//         return res.json();
+//       } else {
+//         throw new Error('Unable to fetch latitude and longitude data');
+//       }
+//     })
+//     .then(data => {
+//       return data.results[0].geometry;
+//     });
+// };
 
-const getLatAndLon = (city, country) => {
-  return fetch(
+const getLatAndLon = async (city, country) => {
+  const response = await fetch(
     `https://api.opencagedata.com/geocode/v1/json?q=${city}+${country}&key=1329eac7d692480f865fcecd32eee0ae&pretty=1`
-  )
-    .then(res => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error('Unable to fetch latitude and longitude data');
-      }
-    })
-    .then(data => {
-      return data.results[0].geometry;
-    });
+  );
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.results[0].geometry;
+  } else {
+    throw new Error('Unable to fetch latitude and longitude data');
+  }
 };
 
-// async function getTimeZone(lat, lon) {
-//   const res = await fetch(
+// const getTimeZone = (lat, lon) => {
+//   return fetch(
 //     `https://dev.virtualearth.net/REST/v1/timezone/${lat},${lon}?key=Au3EwanrpGrNR8JnlaXmkbk8nFFOA2Pcyv1jIp56gyB6TZm6N6XLpXdnwAJcapAe`
-//   );
-//   const data = await res.json();
-//   return data;
-// }
+//   ).then(res => {
+//     if (res.status === 200) {
+//       return res.json();
+//     } else {
+//       throw new Error('Unable to fetch time zones data');
+//     }
+//   });
+// };
 
-const getTimeZone = (lat, lon) => {
-  return fetch(
+const getTimeZone = async (lat, lon) => {
+  const response = await fetch(
     `https://dev.virtualearth.net/REST/v1/timezone/${lat},${lon}?key=Au3EwanrpGrNR8JnlaXmkbk8nFFOA2Pcyv1jIp56gyB6TZm6N6XLpXdnwAJcapAe`
-  ).then(res => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      throw new Error('Unable to fetch time zones data');
-    }
-  });
+  );
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    throw new Error('Unable to fetch time zones data');
+  }
 };
 
-// async function getTime(lat, lon) {
-//   const res = await fetch(
+// const getTime = (lat, lon) => {
+//   return fetch(
 //     `http://api.timezonedb.com/v2.1/get-time-zone?key=RMUYV0BGPUY9&format=json&by=position&lat=${lat}&lng=${lon}`
-//     );
-//   const data = await res.json();
-//   return data;
-// }
+//   ).then(res => {
+//     if (res.status === 200) {
+//       return res.json();
+//     } else {
+//       throw new Error('Unable to fetch time data');
+//     }
+//   });
+// };
 
-const getTime = (lat, lon) => {
-  return fetch(
+const getTime = async (lat, lon) => {
+  const response = await fetch(
     `http://api.timezonedb.com/v2.1/get-time-zone?key=RMUYV0BGPUY9&format=json&by=position&lat=${lat}&lng=${lon}`
-  ).then(res => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      throw new Error('Unable to fetch time data');
-    }
-  });
+  );
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    throw new Error('Unable to fetch time data');
+  }
 };
 
-// async function getCovidData() {
-//   const res = await fetch('https://covid19.mathdro.id/api/confirmed');
-//   const data = await res.json();
-//   return data;
-// }
+// const getCovidData = () => {
+//   return fetch('https://covid19.mathdro.id/api/confirmed').then(res => {
+//     if (res.status === 200) {
+//       return res.json();
+//     } else {
+//       throw new Error('Unable to fetch Covid-19 data');
+//     }
+//   });
+// };
 
-const getCovidData = () => {
-  return fetch('https://covid19.mathdro.id/api/confirmed').then(res => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      throw new Error('Unable to fetch Covid-19 data');
-    }
-  });
+const getCovidData = async () => {
+  const response = await fetch('https://covid19.mathdro.id/api/confirmed');
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    throw new Error('Unable to fetch COVID-19 data');
+  }
 };
